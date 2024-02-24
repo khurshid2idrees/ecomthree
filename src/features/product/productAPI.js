109437fd5264e3ee1bca6f0ba216465830219213
Khurshid2idrees <umarkhurshid3@gmail.com>
2024-02-24 19:37:00 +0530

    Part: 7 (update in productlist productapi and productslice)

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -8,18 +8,27 @@ export function fetchAllProducts() {
   });
 }
 
-export function fetchProductsByFilters(filter) {
-  // filter = {"category":"smartphone"}
+export function fetchProductsByFilters(filter, sort) {
+  // filter = {"category":"smartphone","laptops"}
+  // sort = {_sort:"price,_order="desc"}
   // TODO: on server we will support multi values
   let queryString = "";
   for (let key in filter) {
-    queryString += `${key}=${filter[key]}&`;
+    const categoryValues = filter[key];
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
+  }
+
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
   }
 
   return new Promise(async (resolve) => {
     // TODO: we will not hardcode server URL here
     const response = await fetch(
-      "http://localhost:8080/products?"+queryString
+      "http://localhost:8080/products?" + queryString
     );
     const data = await response.json();
     resolve({ data });
